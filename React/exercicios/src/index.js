fcc3917ce687ae15182d951b169d16faebfb00fe
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2021-01-02 20:20:42 -0300

    Exercicios React

---
+++ React/exercicios/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Saudacao from './componentes/Saudacao'
+
+
+ReactDOM.render(
+    <div>
+        <Saudacao tipo="Bom dia" nome="Joao"/>
+    </div>
+    , document.getElementById('root')
+)
+
+// 
