b35f2259c364c7ee797a2c5903324aedfc443277
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2021-01-02 21:51:24 -0300

    Fim dos exercicios basicos

--- React/exercicios/src/index.js
+++ React/exercicios/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Saudacao from './componentes/Saudacao'
-
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Saudacao tipo="Bom dia" nome="Joao"/>
+        <Pai nome="Paulo" sobrenome="Silva">
+            <Filho nome="Craudin"/>
+            <Filho nome="Jose"/>
+            <Filho nome="Maises"/>
+            <Filho nome="Zé"/>
+        </Pai>
     </div>
     , document.getElementById('root')
 )
