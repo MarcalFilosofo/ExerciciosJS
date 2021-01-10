136905776831aed1bf88094bc309f0a191baae5d
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2021-01-10 11:20:52 -0300

    Final Projeto CRUD
    CRUD, Responsivo

--- React/crud/frontend/src/components/template/Footer.jsx
+++ React/crud/frontend/src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Rodapé
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Guilherme Marçal</strong>
+        </span>
     </footer>
--- React/crud/frontend/src/components/template/Logo.jsx
+++ React/crud/frontend/src/components/template/Logo.jsx
@@ -1,7 +1,11 @@
 import './Logo.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../img/dog.svg'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <Link to="/" className="logo">
+            <img src={logo} alt="logo" height="100px"/>
+        </Link>
     </aside>
