b9f642e3469e1881c548a6baadbd680406bca685
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2021-02-18 15:33:21 -0300

    20210218

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, () => {
+app.all('/opa',(req, res) => {
+    // res.send('Welcome')
+    
+    // res.json({
+    //     name: 'iPad 32GB',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+
+    res.json([{
+        id: 7,
+        name: 'Ana',
+        position: 1
+    }, {
+        id: 8,
+        name: 'Mateus',
+        position: 2
+    }])
+})
+
+
+app.listen(8080, () => {
     console.log('Executando')
 })
